c361eb9e3772326c34501dea10525fd22319f785
newericg1~ <eric.gomes0105@gmail.com>
2024-08-13 20:22:59 -0300

    error build test

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 import { withSentryConfig } from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
+    
+    
 };
 
 export default withSentryConfig(nextConfig, {
