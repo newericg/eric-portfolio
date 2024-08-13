6738e77987173c375b32ab1b6deb3594a015f9c2
newericg1~ <eric.gomes0105@gmail.com>
2024-08-13 19:47:08 -0300

    build fixes

--- data/index.ts
+++ data/index.ts
@@ -173,34 +173,27 @@ export const navItems = [
   export const workExperience = [
     {
       id: 1,
-      title: "Junior Frontend Engineer",
+      title: "Application Developer at IBM",
+      desc: "Active participation in the creation and development of improvements in projects and applications for important clients such as IBM-Itaú and IBM-Santander.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 2,
+      title: "Web Developer at Espresso Labs",
       desc: "Assisted in the development of a web-based platform using Angular, enhancing interactivity.",
       className: "md:col-span-2",
       thumbnail: "/exp1.svg",
     },
     {
-      id: 2,
-      title: "Mobile App Dev - JSM Tech",
-      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-      className: "md:col-span-2", // change to md:col-span-2
-      thumbnail: "/exp2.svg",
-    },
-    {
       id: 3,
       title: "Freelance App Dev Project",
-      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-      className: "md:col-span-2", // change to md:col-span-2
+      desc: "Dev of a web app for a client, from initial concept to live deployment",
+      className: "md:col-span-2",
       thumbnail: "/exp3.svg",
     },
     {
       id: 4,
-      title: "Lead Frontend Developer",
-      desc: "Developed and maintained user-facing features using modern frontend technologies.",
-      className: "md:col-span-2",
-      thumbnail: "/exp4.svg",
-    },
-    {
-      id: 5,
       title: "Lead Frontend Developer",
       desc: "Developed and maintained user-facing features using modern frontend technologies.",
       className: "md:col-span-2",
@@ -215,10 +208,6 @@ export const navItems = [
     },
     {
       id: 2,
-      img: "/twit.svg",
-    },
-    {
-      id: 3,
       img: "/link.svg",
     },
   ];
